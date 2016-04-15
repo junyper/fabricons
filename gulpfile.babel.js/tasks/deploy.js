b0cd85f6aa49dc5d9dded5a2094ffdf655bd52b1
Jennifer Stern <jstern@instructure.com>
2016-04-15 13:17:17 -0500

    Fix deploy task so that it runs build before webpack
    Change-Id: Ia27093b23b32bb77c331c6e23fa1ff1eecb547f3

--- gulpfile.babel.js/tasks/deploy.js
+++ gulpfile.babel.js/tasks/deploy.js
@@ -1,8 +1,13 @@
 import gulp from 'gulp';
 import config from '../config';
-import pages from 'gulp-gh-pages'
+import pages from 'gulp-gh-pages';
+import sequence from 'gulp-sequence';
 
-gulp.task('deploy', ['build', 'webpack'], function () {
+gulp.task('pre-deploy', function (cb) {
+  sequence('build', 'webpack', cb)
+})
+
+gulp.task('deploy', ['pre-deploy'], function () {
   return gulp.src(config.destination + '**/*')
     .pipe(pages())
 });
