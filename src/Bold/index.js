821b6f0f3c89721d332e17da350983159c89914a
Jennifer Stern <jstern@instructure.com>
2016-04-15 13:16:29 -0500

    Update 2016-04-15T18:16:26.032Z
    Change-Id: I7ac2bcecc9941b851f96558c030f73624c7b762b

--- src/Bold/index.js
+++ src/Bold/index.js
@@ -2,3 +2,4 @@ export IconBoldAdd from './IconBoldAdd.js';
 export IconBoldBack from './IconBoldBack.js';
 export IconBoldCheck from './IconBoldCheck.js';
 export IconBoldClose from './IconBoldClose.js';
+export IconBoldTwitter from './IconBoldTwitter.js';
--- src/Light/index.js
+++ src/Light/index.js
@@ -2,4 +2,3 @@ export IconLightAdd from './IconLightAdd.js';
 export IconLightBack from './IconLightBack.js';
 export IconLightCheck from './IconLightCheck.js';
 export IconLightClose from './IconLightClose.js';
-export IconLightTwitter from './IconLightTwitter.js';
